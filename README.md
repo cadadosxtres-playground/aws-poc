b1934608fec5dea5a212bcaf25ad6ff975979362
fcojperez <fcojperez@gmail.com>
2024-07-31 09:15:30 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# AWS PoC
+
+A script to list the aws accounts
+
+## Requirements
+* A python environment with all libraries installed defined in [requirements.txt](./requirements.txt)
+* An AWS Organization
+* A group or user with the policy [AWSOrganizationsReadOnlyAccess](https://docs.aws.amazon.com/aws-managed-policy/latest/reference/AWSOrganizationsReadOnlyAccess.html) configured
---
+++ main.py
@@ -0,0 +1,59 @@
+import boto3
+import argparse
+
+def list_aws_accounts(profile_name: str) -> list:
+    # Create a boto3 session using the specified AWS SSO profile
+    session = boto3.Session(profile_name=profile_name)
+
+    try:
+        # Create a client for the AWS Organizations service
+        client = boto3.client('organizations')
+
+        # Initialize a list to store account information
+        accounts = []
+
+        # Paginate through the list_accounts API to retrieve all accounts
+        paginator = client.get_paginator('list_accounts')
+        for page in paginator.paginate():
+            for account in page['Accounts']:
+                accounts.append({
+                    'AccountId': account['Id'],
+                    'AccountName': account['Name']
+                })
+
+        return accounts
+    except Exception as e:
+        print(f'ERROR!!: Something unexpected happend. More information bellow:\n'
+              f'{e}\n'
+              f'Please check your permissions'
+              )
+
+def arguments() -> object:
+    parser = argparse.ArgumentParser(description='A helper script to gather information from AWS accounts')
+
+    parser.add_argument('-p', '--profile', type=str, default='default', help='Provide an aws-cli profile')
+
+    args = parser.parse_args()
+    return args
+
+
+
+def main():
+
+    args = arguments()
+    try:
+        if args.profile:
+            # Get the list of accounts
+            accounts = list_aws_accounts(profile_name=args.profile)
+
+            if accounts is None:
+                raise Exception(f'ERROR!!! getting list of aws accounts')
+            # Print account details
+            print(f'Account ID, Account Name')
+            for account in accounts:
+                print(f"{account['AccountId']}, {account['AccountName']}")
+    except Exception as e:
+        print(f'{e}')
+
+if __name__ == "__main__":
+    main()
